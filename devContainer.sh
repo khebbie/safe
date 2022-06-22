85d6885f548549c5d8729bc8661aad45b794898c
Klaus Hebsgaard <klaus@hebsgaard.dk>
2022-06-22 08:50:11 +0200

    start devcontainer if exists

--- devContainer.sh
+++ devContainer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -v $(pwd):/app/  safe-dev-container bash
+docker start -i safe-dev-container || docker run -it --name safe-dev-container -v $(pwd):/app/  safe-dev-container bash
